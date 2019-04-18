863358ff31b05a2f164d00954a2148bbf5cdf38f
Fernando Ruiz Berciano <ruizbolyt@gmail.com>
2019-04-18 16:42:48 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ sdi608-lab-soap-sw/src/main/java/com/uniovi/Sdi608LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi608LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi608LabSoapSwApplication.class, args);
+	}
+
+}
